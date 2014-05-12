e0ccdcfbe983c891f9bbc57817b74c7da239c639
Dusan1 <marin.dusan2@gmail.com>
2014-05-12 16:54:58 +0200

    First commit

---
+++ Novi_POS/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Novi_POS
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKorIme = new System.Windows.Forms.TextBox();
+            this.lblKorIme = new System.Windows.Forms.Label();
+            this.lblLozinka = new System.Windows.Forms.Label();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.lblMolimUnesi = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtKorIme
+            // 
+            this.txtKorIme.Location = new System.Drawing.Point(117, 75);
+            this.txtKorIme.Name = "txtKorIme";
+            this.txtKorIme.Size = new System.Drawing.Size(159, 20);
+            this.txtKorIme.TabIndex = 0;
+            // 
+            // lblKorIme
+            // 
+            this.lblKorIme.AutoSize = true;
+            this.lblKorIme.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblKorIme.Location = new System.Drawing.Point(4, 75);
+            this.lblKorIme.Name = "lblKorIme";
+            this.lblKorIme.Size = new System.Drawing.Size(107, 18);
+            this.lblKorIme.TabIndex = 1;
+            this.lblKorIme.Text = "Korisnicko Ime";
+            // 
+            // lblLozinka
+            // 
+            this.lblLozinka.AutoSize = true;
+            this.lblLozinka.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblLozinka.Location = new System.Drawing.Point(51, 106);
+            this.lblLozinka.Name = "lblLozinka";
+            this.lblLozinka.Size = new System.Drawing.Size(60, 18);
+            this.lblLozinka.TabIndex = 3;
+            this.lblLozinka.Text = "Lozinka";
+            // 
+            // txtLozinka
+            // 
+            this.txtLozinka.Location = new System.Drawing.Point(117, 107);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.Size = new System.Drawing.Size(159, 20);
+            this.txtLozinka.TabIndex = 2;
+            // 
+            // lblMolimUnesi
+            // 
+            this.lblMolimUnesi.AutoSize = true;
+            this.lblMolimUnesi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblMolimUnesi.Location = new System.Drawing.Point(58, 21);
+            this.lblMolimUnesi.Name = "lblMolimUnesi";
+            this.lblMolimUnesi.Size = new System.Drawing.Size(179, 24);
+            this.lblMolimUnesi.TabIndex = 4;
+            this.lblMolimUnesi.Text = "Molimo ulogujte se! ";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnOk.Location = new System.Drawing.Point(117, 133);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(159, 34);
+            this.btnOk.TabIndex = 5;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 187);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lblMolimUnesi);
+            this.Controls.Add(this.lblLozinka);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.lblKorIme);
+            this.Controls.Add(this.txtKorIme);
+            this.Name = "loginForm";
+            this.Text = "Kafe Pro 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKorIme;
+        private System.Windows.Forms.Label lblKorIme;
+        private System.Windows.Forms.Label lblLozinka;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.Label lblMolimUnesi;
+        private System.Windows.Forms.Button btnOk;
+
+
+    }
+}
+
---
+++ Novi_POS/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Novi_POS
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        Database1Entities dbe = new Database1Entities();
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (txtKorIme.Text == "" || txtLozinka.Text == "")
+            {
+                MessageBox.Show("Please enter username and password.");
+                return;
+            }
+
+            tblKorisnici l = new tblKorisnici();
+
+            foreach (var p in dbe.tblKorisnici)
+            {
+                if (p.korisnickoIme == txtKorIme.Text && p.lozinka == txtLozinka.Text)
+                {
+                    MessageBox.Show("Successfully Logged In.");
+
+                    SadrzajForma c = new SadrzajForma();
+
+                    //var query = from g in dbe.tblKorisnici where g.korisnickoIme == txtKorIme.Text select g;
+
+                    //var queryList = query.ToList();
+
+                    c.label1.Text = "Zdravo, " + p.korisnickoIme;
+
+                    c.Show();
+
+                    return;
+                }
+            }
+            foreach (var p in dbe.tblKorisnici)
+            {
+                if (p.korisnickoIme != txtKorIme.Text || p.lozinka != txtLozinka.Text)
+                {
+                    MessageBox.Show("Wrong Username and/or Password.");
+                    return;
+                }
+            }
+        }
+    }
+}
